4cb6832ca712b327a0401b53524626367d7844aa
azuniga5 <azuniga5@vcd-ch204-21.eastern.ewu.edu>
2017-05-25 14:36:30 -0700

    added missing fonts

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 // Scripts
+//Banner Images
+var img0 = 'https://unsplash.it/600/300?image=1';
+var img1 = 'https://unsplash.it/600/300?image=2';
+var img2 = 'https://unsplash.it/600/300?image=3';
+
+// Random Number Generator
+var maxRand = 3;
+var randNum = Math.floor( Math.random() * maxRand );
+console.log(randNum);
+
+//swap out banner
+$('header img').atrr('src',  eval ('img1' + randNum) );
+
 
 // Lightgallery
 $('figure').lightGallery({
