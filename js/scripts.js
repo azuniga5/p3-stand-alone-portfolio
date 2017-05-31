9b6462ca56bc945f112d7c0bff64da8bd21b68d7
azuniga5 <azuniga5@vcd038841md.eastern.ewu.edu>
2017-05-30 21:49:32 -0700

    fixed mistakes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Scripts
-//Banner Images
+// Store Banner Images
 var img0 = 'https://unsplash.it/600/300?image=1';
 var img1 = 'https://unsplash.it/600/300?image=2';
 var img2 = 'https://unsplash.it/600/300?image=3';
@@ -9,12 +9,15 @@ var maxRand = 3;
 var randNum = Math.floor( Math.random() * maxRand );
 console.log(randNum);
 
-//swap out banner
-$('header img').atrr('src',  eval ('img1' + randNum) );
-
+// Swap Out Banner
+$('header img').attr('src', eval('img' + randNum) );
 
 // Lightgallery
 $('figure').lightGallery({
     selector: 'a', 
-    mode: 'lg-lollipop'
+    mode: 'lg-lollipop',
+    thumbnail: true
 });
+
+// Random Background
+$('body').addClass( 'bg' + randNum );
